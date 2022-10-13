b86bd8f2a13435c2067cd38f5dd511e4ed667194
unknown <joaoo.bio@gmail.com>
2022-10-13 19:09:09 -0300

    Updates

---
+++ component---src-pages-404-js-47876727c86c8cc4c4a6.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{429:function(t,e,n){n.r(e),n.d(e,{Head:function(){return l}});var u=n(7294),a=n(2385),r=n(9357),l=function(){return u.createElement(r.Z,{title:"404: Not Found"})};e.default=function(){return u.createElement(a.Z,null,u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-47876727c86c8cc4c4a6.js.map
